523d394cab8adb995584ef5f24b184bff8d30de5
Sarkhan Rasimoghlu <nasibovsarxan92@gmail.com>
2022-03-14 02:32:34 +0400

    Edited composer.json file

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,6 +22,8 @@ class ComposerAutoloaderInitcfebafcacc09383c9b5b9e5e937f3177
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInitcfebafcacc09383c9b5b9e5e937f3177', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(\dirname(__FILE__)));
         spl_autoload_unregister(array('ComposerAutoloaderInitcfebafcacc09383c9b5b9e5e937f3177', 'loadClassLoader'));
